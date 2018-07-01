63e63dbf9867e36ae295cfce87f65c42ff778f5e
Xiaolei Wu <xiaolei.wu@mail.utoronto.ca>
2018-07-01 12:34:07 -0400

    Create User.java attributes.

--- phase1/src/User.java
+++ phase1/src/User.java
@@ -1,3 +1,11 @@
+
+import java.util.ArrayList;
+
 public class User {
-    
+    private String userName;
+    private String emailAddress;
+    private ArrayList<Card> myCards;
+    private String password;
+
+
 }
