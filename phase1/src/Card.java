21465fe2752f0e69ee8b7bee377e9522fc202e74
Xiaolei Wu <xiaolei.wu@mail.utoronto.ca>
2018-07-02 15:16:31 -0400

    create constructor and setter for User for class Card.

--- phase1/src/Card.java
+++ phase1/src/Card.java
@@ -1,7 +1,19 @@
+import java.util.ArrayList;
+
 public class Card {
     private boolean isSuspended = false;
     private int id;
+    private User user;
+    private double balance;
+    private static int idIncrementer = 1000;
+    private ArrayList<Trip> myTrip;
 
+
+    Card(){
+        this.id = idIncrementer;
+        idIncrementer++;
+        this.balance = 19;
+    }
 
     void reverseSuspended(){
         this.isSuspended = !this.isSuspended;
@@ -9,5 +21,9 @@ public class Card {
 
     int getId(){
         return this.id;
+    }
+
+    void setUser(User user){
+        this.user = user;
     }
 }
