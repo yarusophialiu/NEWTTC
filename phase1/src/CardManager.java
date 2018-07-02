1380c0077d8800a8b929fa58fde06a34278099ec
shiyuxin <yuxinsteven@gmail.com>
2018-07-02 15:48:18 -0400

    Adding attributes and methods

---
+++ phase1/src/CardManager.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class CardManager {
+    private ArrayList<Card> cardList = new ArrayList<Card>();
+
+    void addCard(Card card){cardList.add(card);
+    }
+
+    ArrayList getCardList(){
+        return cardList;
+    }
+
+    void deleteCard(Long id){
+        for (int i = 0; i < cardList.size();i++){
+            if (cardList.get(i).getId() == id){
+                cardList.remove(i);
+            }
+        }
+    }
+}
