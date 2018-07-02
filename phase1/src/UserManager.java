15642ad825c643a9d0db7dfd31afda15b67b6183
Yunfan Zhang <yunfanzhang.zhang@mail.utoronto.ca>
2018-07-02 15:19:33 -0400

    Created a new class UserManager and added a new method addUser.

---
+++ phase1/src/UserManager.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class UserManager {
+    private ArrayList<User> Userlist = new ArrayList<>();
+
+
+    void addUser(User user){
+        Userlist.add(user);
+    }
+
+
+}
