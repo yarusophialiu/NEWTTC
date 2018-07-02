ffd45d5ca96acd39a4c6dc1959a75f5328889e86
Xiaolei Wu <xiaolei.wu@mail.utoronto.ca>
2018-07-02 15:45:58 -0400

    add getter and setter for Trip.java.

--- phase1/src/Trip.java
+++ phase1/src/Trip.java
@@ -6,10 +6,42 @@ public class Trip {
     private Time enterTime;
     private Time exitTime;
     private String transportation;    //vehicles used in this trip.
+    private boolean isContinuous = false;
+    private double currentFare;
+    private Time continuousTime;
 
     Trip(String entrance, Time enterTime, String vehile){
         this.enterTime = enterTime;
         this.entrance = entrance;
         this.transportation = vehile;
+    }
+
+    void setExit(String exit, Time exitTime){
+        this.exit = exit;
+        this.exitTime = exitTime;
+    }
+
+    void reverseContinuous(){
+        this.isContinuous = !this.isContinuous;
+    }
+
+    void setCurrentFare(double fare){
+        this.currentFare = fare;
+    }
+
+    void setContinuousTime(Time time){
+        this.continuousTime = time;
+    }
+
+    double getCurrentFare(){
+        return this.currentFare;
+    }
+
+    Time getContinuousTime(){
+        return continuousTime;
+    }
+
+    boolean getIsContinuous(){
+        return this.isContinuous;
     }
 }
