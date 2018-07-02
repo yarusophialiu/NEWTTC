cf5f44304e381dc57066cb16ffafd918fa172bba
Yunfan Zhang <yunfanzhang.zhang@mail.utoronto.ca>
2018-07-02 15:41:24 -0400

    Added a new method removeUser to class UserManager.

--- phase1/src/UserManager.java
+++ phase1/src/UserManager.java
@@ -8,5 +8,13 @@ public class UserManager {
         Userlist.add(user);
     }
 
+    void removeUser(User user){
+        for (int i = 0; i < Userlist.size(); i++){
+            if (Userlist.get(i).getEmailAddress().equals(user.getEmailAddress())){
+                Userlist.remove(i);
+            }
+        }
+    }
+
 
 }
