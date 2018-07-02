faec9296b408466e63785144c9702c7ba016b01b
Xiaolei Wu <xiaolei.wu@mail.utoronto.ca>
2018-07-02 14:36:29 -0400

    add basic method for User class and corresponding method in Card class.

--- phase1/src/Card.java
+++ phase1/src/Card.java
@@ -1,2 +1,13 @@
 public class Card {
+    private boolean isSuspended = false;
+    private int id;
+
+
+    void reverseSuspended(){
+        this.isSuspended = !this.isSuspended;
+    }
+
+    int getId(){
+        return this.id;
+    }
 }
--- phase1/src/User.java
+++ phase1/src/User.java
@@ -7,5 +7,38 @@ public class User {
     private ArrayList<Card> myCards;
     private String password;
 
+    User(String userName, String emailAddress, String password){
+        this.userName = userName;
+        this.emailAddress = emailAddress;
+        myCards = new ArrayList<Card>();
+        this.password = password;
+    }
+
+    void buyCard(){
+        Card card = new Card();  // modify the constructor later when finished Card class.
+        addCard(card);
+
+    }
+
+    void addCard(Card card){
+        if(!myCards.contains(card)){
+            this.myCards.add(card);
+        }
+        else{
+            System.out.println("This card was already added.");
+        }
+    }
+
+    void lostCard(int id){
+        for (Card card : myCards){
+            if(card.getId() == id){
+                card.reverseSuspended();
+            }
+        }
+    }
+
+    void changeName(String newName){
+        this.userName = newName;
+    }
 
 }
