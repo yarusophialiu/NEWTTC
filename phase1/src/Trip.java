6dbc54b34cc1726ea3db2e12ef0f211b61e415d0
yaruliu <yarusophia@gmail.com>
2018-07-03 10:19:36 -0400

    create Trip.java and its attributes

---
+++ phase1/src/Trip.java
@@ -0,0 +1,10 @@
+import java.sql.Time;
+
+public class Trip {
+    private String entrance;
+    private String exit;
+    private Time enterTime;
+    private Time exitTime;
+    private boolean isContinous = false;
+    private boolean currentFare;
+}
