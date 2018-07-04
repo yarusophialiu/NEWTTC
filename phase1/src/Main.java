5ddcc987a40326b589976fb5359505e1a0c60a2f
Xiaolei Wu <xiaolei.wu@mail.utoronto.ca>
2018-07-04 14:45:35 -0400

    create a simple test file reader for Main.java

--- phase1/src/Main.java
+++ phase1/src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-  public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Main {
+  public static void main(String[] args) throws IOException{
+        BufferedReader fileReader = new BufferedReader(new FileReader("stations.txt"));
+        String info = fileReader.readLine();
+        System.out.println(info);
   }
 }
