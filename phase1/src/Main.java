533dee5ae4d6763a168fa2fd267d3a30c194e1cf
shiyuxin <yuxinsteven@gmail.com>
2018-07-04 16:40:05 -0400

    for push

--- phase1/src/Main.java
+++ phase1/src/Main.java
@@ -3,9 +3,24 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Main {
-  public static void main(String[] args) throws IOException{
-        BufferedReader fileReader = new BufferedReader(new FileReader("stations.txt"));
+    public static void main(String[] args) throws IOException{
+        BufferedReader fileReader = new BufferedReader(new FileReader("phase1/src/stations.txt"));
         String info = fileReader.readLine();
-        System.out.println(info);
-  }
+        info = fileReader.readLine();
+        while (!info.equals("")) {
+            new Station(info);
+            info = fileReader.readLine();
+        }
+        info = fileReader.readLine();
+        info = fileReader.readLine();
+        while (!(info == null)){
+            new Stop(info);
+            info = fileReader.readLine();
+        }
+        // the above parts are for reading from station.txt
+
+        BufferedReader fileReader2 = new BufferedReader(new FileReader("phase1/src/events.txt"));
+        String info2 = fileReader2.readLine();
+
+    }
 }
