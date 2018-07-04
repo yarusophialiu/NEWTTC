46b9a13e0616790da93c8270f940cfc072584831
Xiaolei Wu <xiaolei.wu@mail.utoronto.ca>
2018-07-04 16:17:35 -0400

    read station and stop information from stations.txt

--- phase1/src/Main.java
+++ phase1/src/Main.java
@@ -4,8 +4,19 @@ import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) throws IOException{
-        BufferedReader fileReader = new BufferedReader(new FileReader("stations.txt"));
+        BufferedReader fileReader = new BufferedReader(new FileReader("phase1/src/stations.txt"));
         String info = fileReader.readLine();
-        System.out.println(info);
+        info = fileReader.readLine();
+        while (!info.equals("")) {
+            new Station(info);
+            info = fileReader.readLine();
+        }
+        info = fileReader.readLine();
+        info = fileReader.readLine();
+        while (!(info == null)){
+            new Stop(info);
+            info = fileReader.readLine();
+        }
+
   }
 }
