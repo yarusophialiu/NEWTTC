11197f1c2aacc921897fb256afa29ececa0843ac
Xiaolei Wu <xiaolei.wu@mail.utoronto.ca>
2018-07-04 15:51:24 -0400

    create getters and variables for Stop.java and Station.java

--- phase1/src/Station.java
+++ phase1/src/Station.java
@@ -1,6 +1,19 @@
-public class Station {
+import java.util.ArrayList;
+
+class Station {
     /**
      * this class is used to record stations for subway.
      */
     private String name;
+
+    private static ArrayList<String> stations = new ArrayList<>();
+
+    Station(String name){
+        this.name = name;
+        Station.stations.add(name);
+    }
+
+    static ArrayList<String> getStations() {
+        return stations;
+    }
 }
--- phase1/src/Stop.java
+++ phase1/src/Stop.java
@@ -1,6 +1,19 @@
-public class Stop {
+import java.util.ArrayList;
+
+class Stop {
     /**
      * this class is used to record stops for bus.
      */
     private String name;
+
+    private static ArrayList<String> stops = new ArrayList<>();
+
+    Stop(String name){
+        this.name = name;
+        Stop.stops.add(name);
+    }
+
+    static ArrayList<String> getStops() {
+        return stops;
+    }
 }
