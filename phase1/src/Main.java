6ca71b2ae6f1e4960cd57c50203d4b557f571fc4
yaruliu <yarusophia@gmail.com>
2018-07-05 20:38:33 -0400

    solve merge conflict

--- phase1/src/Main.java
+++ phase1/src/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) throws IOException{
-        BufferedReader fileReader = new BufferedReader(new FileReader("stations.txt"));
+        BufferedReader fileReader = new BufferedReader(new FileReader("phase1/src/stations.txt"));
         String info = fileReader.readLine();
         System.out.println(info);
   }
