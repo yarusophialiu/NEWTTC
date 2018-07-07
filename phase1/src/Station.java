0c27d4dd4f4a0b71b046e81d0d037e2d1dc0ea3b
Xiaolei Wu <xiaolei.wu@mail.utoronto.ca>
2018-07-07 16:29:16 -0400

    modify Station class to fit into the Dijktra's algorithm

--- phase1/src/Station.java
+++ phase1/src/Station.java
@@ -4,16 +4,41 @@ class Station {
     /**
      * this class is used to record stations for subway.
      */
-    private String name;
-
     private static ArrayList<String> stations = new ArrayList<>();
+    private String name;
+    private ArrayList<Station> neighbour = new ArrayList<>();
+    private Station pathVia;
+    private int distance;
+
+    Station(String name, ArrayList<Station> neighbour){
+        this.name = name;
+        Station.stations.add(name);
+        this.neighbour = neighbour;
+    }
 
     Station(String name){
         this.name = name;
         Station.stations.add(name);
     }
 
+
     static ArrayList<String> getStations() {
         return stations;
+    }
+
+    Station getPathVia(){
+        return pathVia;
+    }
+
+    void setPathVia(Station pathVia){
+        this.pathVia = pathVia;
+    }
+
+    ArrayList<Station> getNeighbour(){
+        return this.neighbour;
+    }
+
+    void setNeighbour(ArrayList<Station> neighbour){
+        this.neighbour = neighbour;
     }
 }
