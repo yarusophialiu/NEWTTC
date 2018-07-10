c26f2953228486e5ad57b525f9ae98d7d3a7f8d1
Xiaolei Wu <xiaolei.wu@mail.utoronto.ca>
2018-07-10 14:45:30 -0400

    read from stations.txt and update all stations and stops

--- phase1/src/Main.java
+++ phase1/src/Main.java
@@ -1,13 +1,24 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) throws IOException{
         BufferedReader fileReader = new BufferedReader(new FileReader("phase1/src/stations.txt"));
         String info = fileReader.readLine();
+        info = fileReader.readLine();
         while (!info.equals("")) {
-            StationManager.addStations(info);
+            String[] data = info.split(" ");
+            ArrayList<String> dataArray = new ArrayList<String>(Arrays.asList(data));
+            dataArray.remove(dataArray.get(1));
+            for (String id : dataArray) {
+                Station station = StationManager.newStation(id);
+                for (int i = 1; i < dataArray.size(); i++){
+                    station.addNeighbours(StationManager.newStation(dataArray.get(i)));
+                }
+            }
             info = fileReader.readLine();
         }
         info = fileReader.readLine();
