b3d7a5646d2cdfef9e0dd725a0389649d48f7a29
Xiaolei Wu <xiaolei.wu@mail.utoronto.ca>
2018-07-10 14:47:54 -0400

    fixing error from reading disk

--- phase1/src/Main.java
+++ phase1/src/Main.java
@@ -6,13 +6,10 @@ import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) throws IOException{
-        StationManager stationManager = new StationManager();
-        StopManager stopManager = new StopManager();
         BufferedReader fileReader = new BufferedReader(new FileReader("phase1/src/stations.txt"));
         String info = fileReader.readLine();
         info = fileReader.readLine();
         while (!info.equals("")) {
-<<<<<<< HEAD
             String[] data = info.split(" ");
             ArrayList<String> dataArray = new ArrayList<String>(Arrays.asList(data));
             dataArray.remove(dataArray.get(1));
@@ -22,20 +19,12 @@ public class Main {
                     station.addNeighbours(StationManager.newStation(dataArray.get(i)));
                 }
             }
-=======
-            stationManager.addStations(info);
->>>>>>> b9cab4c1d5fd5a59fb60449edb5376a2fd5065a9
             info = fileReader.readLine();
         }
         info = fileReader.readLine();
         while (!(info == null)){
-            stopManager.addStop(info);
+            StopManager.addStop(info);
             info = fileReader.readLine();
         }
-        // the above parts are for reading from station.txt
-
-        BufferedReader fileReader2 = new BufferedReader(new FileReader("phase1/src/events.txt"));
-        String info2 = fileReader2.readLine();
-
   }
 }
