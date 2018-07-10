3a00be3fa9d9fde8f3da472d58594cc50d3a384d
Xiaolei Wu <xiaolei.wu@mail.utoronto.ca>
2018-07-09 20:36:45 -0400

    make Main to record information from Stations.txt

--- phase1/src/Main.java
+++ phase1/src/Main.java
@@ -4,23 +4,16 @@ import java.io.IOException;
 
 public class Main {
   public static void main(String[] args) throws IOException{
-        StationManager stationManager = new StationManager();
-        StopManager stopManager = new StopManager();
         BufferedReader fileReader = new BufferedReader(new FileReader("phase1/src/stations.txt"));
         String info = fileReader.readLine();
         while (!info.equals("")) {
-            stationManager.addStations(info);
+            StationManager.addStations(info);
             info = fileReader.readLine();
         }
         info = fileReader.readLine();
         while (!(info == null)){
-            stopManager.addStop(info);
+            StopManager.addStop(info);
             info = fileReader.readLine();
         }
-        // the above parts are for reading from station.txt
-
-        BufferedReader fileReader2 = new BufferedReader(new FileReader("phase1/src/events.txt"));
-        String info2 = fileReader2.readLine();
-
   }
 }
