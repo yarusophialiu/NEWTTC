1b70558db1f9c4e95d37205774621e3ed7c185f0
Xiaolei Wu <xiaolei.wu@mail.utoronto.ca>
2018-07-11 14:54:51 -0400

    nothing changed...

--- phase1/src/Main.java
+++ phase1/src/Main.java
@@ -1,42 +1,42 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
-import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
+  /** main method for our program, read stations.txt and record information from them, and read events.txt and pass the
+   * information in it one line after one line to PrestoCardSystem to execute those events.*/
   public static void main(String[] args) throws Exception {
-        BufferedReader fileReader = new BufferedReader(new FileReader("phase1/src/stations.txt"));
+        BufferedReader fileReader = new BufferedReader(new FileReader("stations.txt"));
         String vehicle = fileReader.readLine();
         String info = fileReader.readLine();
-        while (!info.equals("")) {
-            ArrayList<String> dataArray = new ArrayList<String>(Arrays.asList(info.split(" ")));
-            dataArray.remove(dataArray.get(1));
-            Station station = StationManager.newStation(dataArray.get(0), vehicle);
-                for (int i = 1; i < dataArray.size(); i++){
-                    station.addNeighbours(StationManager.newStation(dataArray.get(i), vehicle));
-                }
-            info = fileReader.readLine();
-        }
-        vehicle = fileReader.readLine();
-        info = fileReader.readLine();
         while (!(info == null)){
-            ArrayList<String> dataArray = new ArrayList<String>(Arrays.asList(info.split(" ")));
-            dataArray.remove(dataArray.get(1));
-            Station stop = StationManager.newStation(dataArray.get(0), vehicle);
-            for (int i = 1; i < dataArray.size(); i++){
-                stop.addNeighbours(StationManager.newStation(dataArray.get(i), vehicle));
+            switch (info) {
+                case "":
+                    info = fileReader.readLine();
+                    break;
+                case "bus":
+                    vehicle = "bus";
+                    info = fileReader.readLine();
+                    break;
+                default:
+                    ArrayList<String> dataArray = new ArrayList<>(Arrays.asList(info.split(" ")));
+                    dataArray.remove(dataArray.get(1));
+                    Station station = StationManager.newStation(dataArray.get(0), vehicle);
+                    for (int i = 1; i < dataArray.size(); i++) {
+                        station.addNeighbours(StationManager.newStation(dataArray.get(i), vehicle));
+                    }
+                    info = fileReader.readLine();
+                    break;
             }
-            info = fileReader.readLine();
         }
 
         PrestoCardSystem prestoCardSystem = new PrestoCardSystem();
-        BufferedReader fileReader2 = new BufferedReader(new FileReader("phase1/src/events.txt"));
+        BufferedReader fileReader2 = new BufferedReader(new FileReader("events.txt"));
         String info2 = fileReader2.readLine();
         while (!(info2 == null)){
             if (!info2.equals("")){
-                ArrayList<String> data = new ArrayList<String>(Arrays.asList(info2.split(" ")));
+                ArrayList<String> data = new ArrayList<>(Arrays.asList(info2.split(" ")));
                 prestoCardSystem.doEvent(data);
             }
             info2 = fileReader2.readLine();
