2ae682d924e1722534bc9fc0c8376b65d4274bcf
yaruliu <yarusophia@gmail.com>
2018-07-27 19:03:28 -0400

    add dashboard scene to loginsystem scene

--- phase2/src/prestosystem/controller/LoginSystem.java
+++ phase2/src/prestosystem/controller/LoginSystem.java
@@ -1,10 +1,27 @@
 package prestosystem.controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginSystem {
 
+    // when this method is called, it will change the scene
     public void loginButtonClicked(){
-        System.out.println("ready to switch scene");
-
+        System.out.println("hi");
     }
 
+    public void nextScene(javafx.event.ActionEvent event) throws IOException{
+        Parent dashboard = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+        Scene dashboardScene = new Scene(dashboard);
+
+        //get stage information
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(dashboardScene);
+        window.show();
+    }
 }
