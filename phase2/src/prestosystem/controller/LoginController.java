d147c098a055d5d67ec6d4ff458b561ab2bfbba6
yaruliu <yarusophia@gmail.com>
2018-07-31 03:31:48 -0400

    write new user to user.txt in buycardcontroller

--- phase2/src/prestosystem/controller/LoginController.java
+++ phase2/src/prestosystem/controller/LoginController.java
@@ -1,15 +1,39 @@
 package prestosystem.controller;
-
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LoginController {
+public class LoginController implements Initializable{
+    @FXML
+    private TextField password;
 
+    @FXML
+    private TextField email;
+
+    @FXML
+    void login(javafx.event.ActionEvent event) {
+
+    }
+
+    @FXML
+    void buycard(javafx.event.ActionEvent event) throws IOException{
+        Parent dashboard = FXMLLoader.load(getClass().getResource("buycard.fxml"));
+        Scene dashboardScene = new Scene(dashboard);
+
+        //get stage information
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        window.setScene(dashboardScene);
+        window.show();
+    }
 
     // when this method is called, it will change the scene
     public void nextScene(javafx.event.ActionEvent event) throws IOException{
@@ -20,5 +44,10 @@ public class LoginController {
         Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
         window.setScene(dashboardScene);
         window.show();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
     }
 }
