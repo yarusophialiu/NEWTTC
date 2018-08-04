d3d7ea98e31acb194d2e434c11102ca1d0eb1af1
yaruliu <yarusophia@gmail.com>
2018-08-04 14:19:17 -0400

    fix addcard in dashboard.java

--- phase2/src/controller/Dashboard.java
+++ phase2/src/controller/Dashboard.java
@@ -8,7 +8,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import model.Card;
 import model.RegularUser;
@@ -23,24 +23,17 @@ public class Dashboard extends Controller implements Initializable{
     private ArrayList<Card> cards;
 
     @FXML
-    private GridPane buttonGrid;
-
-    @FXML
     private TextField newUsername;
 
     @FXML
     private Label userName = new Label();
 
+    @FXML
+    private HBox hbox;
+
 
     public void goBackPage(javafx.event.ActionEvent event) throws Exception {
         switchScene(event, "login.fxml");
-    }
-
-    public void goToCard(javafx.event.ActionEvent event) throws Exception {
-//        FXMLLoader loader = new FXMLLoader(getClass().getResource("cardController.fxml"));
-//        CardController cardController = loader.getController();
-//        cardController.setCardSet(cards);
-//        switchScene(event, "cardController.fxml");
     }
 
     void setUser(User newUser){
@@ -55,9 +48,7 @@ public class Dashboard extends Controller implements Initializable{
         Button button = new Button();
         int id = ((RegularUser)user).buyCard();
         button.setText(Integer.toString(id));
-        buttonGrid.add(button, cards.size(), 0);
-
-        //set on action
+        hbox.getChildren().add(button);
     }
 
     @FXML
@@ -78,11 +69,10 @@ public class Dashboard extends Controller implements Initializable{
 
 
     public void loadCard() {
-        int col = 0;
         for (Card card : cards) {
             Button button = new Button(Integer.toString(card.getId()));
             button.setText(Integer.toString(card.getId()));
-            buttonGrid.add(button, col, 0);
+            hbox.getChildren().add(button);
             button.setOnAction(event -> {
                 try {
                     helper(card);
@@ -90,7 +80,6 @@ public class Dashboard extends Controller implements Initializable{
                     e.printStackTrace();
                 }
             });
-            col++;
         }
     }
 
