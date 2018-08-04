42e12e84d7f66e063844473d0c19f779d4eb9f27
yaruliu <yarusophia@gmail.com>
2018-08-04 15:47:10 -0400

    create adminuser controller and fxml

---
+++ phase2/src/controller/AdminuserController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class AdminuserController {
+    @FXML
+    private Text totalStations;
+
+    @FXML
+    private Text totalStops;
+
+    @FXML
+    private Text totalRev;
+}
