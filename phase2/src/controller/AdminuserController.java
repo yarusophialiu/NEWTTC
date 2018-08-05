f485122e3a46d051c2f76baa71c9158c30c29d74
yaruliu <yarusophia@gmail.com>
2018-08-04 20:08:55 -0400

    add loadinfo method in admincontroller

--- phase2/src/controller/AdminuserController.java
+++ phase2/src/controller/AdminuserController.java
@@ -1,20 +1,21 @@
 package controller;
 
+import application.TransitApp;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
 public class AdminuserController extends Controller{
-    @FXML
-    private Text totalStations;
 
     @FXML
-    private Text totalStops;
-
-    @FXML
-    private Text totalRev;
+    private Text dailyReport;
 
     @FXML
     public void goBackPage(javafx.event.ActionEvent event) throws Exception {
         switchScene(event, "login.fxml");
     }
+
+    public void loadInfo() {
+        dailyReport.setText(TransitApp.getAdmin().report());
+    }
+
 }
