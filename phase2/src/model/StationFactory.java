7f7f889d8d9ad2e271f087fb318c13ee3ea3cb38
yaruliu <yarusophia@gmail.com>
2018-08-05 23:00:19 -0400

    fix refactor error

--- phase2/src/model/StationFactory.java
+++ phase2/src/model/StationFactory.java
@@ -6,19 +6,23 @@ public class StationFactory {
     private static HashMap<String, SubwayStation> subwayStationHashMap = new HashMap<>();
     private static HashMap<String, BusStation> busStationHashMap = new HashMap<>();
 
+    private Station newSub(String stationID, String vehicle) {
+        if (subwayStationHashMap.keySet().contains(stationID)) {
+            for (SubwayStation station : subwayStationHashMap.values()) {
+                if (station.getName().equals(stationID)) {
+                    return station;
+                }
+            }
+        }
+        Station station = new SubwayStation(stationID, vehicle);
+        subwayStationHashMap.put(stationID, (SubwayStation) station);
+        return station;
+    }
+
     public Station newStation(String stationID, String vehicle, String lineNumber) {
         Integer lineNum = Integer.parseInt(lineNumber);
         if (vehicle.equals("subway")){
-            if (subwayStationHashMap.keySet().contains(stationID)) {
-                for (SubwayStation station : subwayStationHashMap.values()) {
-                    if (station.getName().equals(stationID)) {
-                        return station;
-                    }
-                }
-            }
-            Station station = new SubwayStation(stationID, vehicle);
-            subwayStationHashMap.put(stationID, (SubwayStation) station);
-            return station;
+            return newSub(stationID, vehicle);
         }
        else{
             if (busStationHashMap.keySet().contains(stationID)) {
