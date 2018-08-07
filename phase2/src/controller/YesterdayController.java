086389f58f45894ab4041f2dcacf2fb048d80175
yaruliu <yarusophia@gmail.com>
2018-08-06 21:11:49 -0400

    add loadinfo in yesterdaycontroller.java

--- phase2/src/controller/YesterdayController.java
+++ phase2/src/controller/YesterdayController.java
@@ -1,8 +1,22 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import model.AdminUser;
+import model.User;
 
 public class YesterdayController extends Controller{
+    @FXML
+    private Text yesterdayReport;
+
+    public void loadInfo() {
+        for (User user : User.getUsers().values()){
+            if (user.getEmailAddress().equals("adminuser@mail.com")){
+                yesterdayReport.setText(((AdminUser)user).yesterdayReport());
+            }
+        }
+    }
 
     public void goBackPage(ActionEvent event) throws Exception{
         switchScene(event, "adminuser.fxml");
