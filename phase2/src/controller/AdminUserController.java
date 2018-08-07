4a424fda7f1ecbbbfd1dcc8d5e7c4d579b4dc713
yaruliu <yarusophia@gmail.com>
2018-08-07 14:19:19 -0400

    add java doc for adminusercontroller.java

--- phase2/src/controller/AdminUserController.java
+++ phase2/src/controller/AdminUserController.java
@@ -4,7 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import model.AdminUser;
@@ -20,6 +19,7 @@ public class AdminUserController extends Controller{
         switchScene(event, "login.fxml");
     }
 
+    /**Load information of adminuser.fxml*/
     public void loadInfo() {
         for (User user : User.getUsers().values()){
             if (user.getEmailAddress().equals("adminuser@mail.com")){
@@ -28,6 +28,11 @@ public class AdminUserController extends Controller{
         }
     }
 
+    /**
+     * Enter and load information of yesterday.fxml when click the button
+     *
+     * @param event the action that happens when we click button
+     */
     public void yesterdayReport(ActionEvent event) throws Exception{
         Stage loginWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
         FXMLLoader loader = loading("yesterday.fxml");
@@ -36,10 +41,20 @@ public class AdminUserController extends Controller{
         loginWindow.close();
     }
 
+    /**
+     * Enter and load information of monthly.fxmll when click the button
+     *
+     * @param event the action that happens when we click button
+     */
     public void monthlyReport(ActionEvent event) throws Exception{
         switchScene(event, "monthly.fxml");
     }
 
+    /**
+     * Enter and load information of yearly.fxml when click the button
+     *
+     * @param event the action that happens when we click button
+     */
     public void yearlyReport(ActionEvent event) throws Exception{
         switchScene(event, "yearly.fxml");
     }
