da3d661eebc1c46c67e85db36849b5da0b11a947
yaruliu <yarusophia@gmail.com>
2018-08-06 21:15:10 -0400

    add yesterdya report method in adminusercontroller.java

--- phase2/src/controller/AdminUserController.java
+++ phase2/src/controller/AdminUserController.java
@@ -2,7 +2,11 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 import model.AdminUser;
 import model.User;
 
@@ -25,7 +29,11 @@ public class AdminUserController extends Controller{
     }
 
     public void yesterdayReport(ActionEvent event) throws Exception{
-        switchScene(event, "yesterday.fxml");
+        Stage loginWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        FXMLLoader loader = loading("yesterday.fxml");
+        YesterdayController yesterdayController= loader.getController();
+        yesterdayController.loadInfo();
+        loginWindow.close();
     }
 
     public void monthlyReport(ActionEvent event) throws Exception{
