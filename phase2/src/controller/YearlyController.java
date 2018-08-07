7c8adc3ea3006653b82744b90fe7c9ac2a68f00b
yaruliu <yarusophia@gmail.com>
2018-08-07 16:24:00 -0400

    display barchart in yearly report

--- phase2/src/controller/YearlyController.java
+++ phase2/src/controller/YearlyController.java
@@ -1,10 +1,38 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
+import model.AdminUser;
+import model.User;
 
-public class YearlyController extends Controller{
+import java.net.URL;
+import java.util.HashMap;
+import java.util.ResourceBundle;
+
+public class YearlyController extends Controller implements Initializable{
+    @FXML
+    private BarChart<?, ?> yearlyChart;
+
 
     public void goBackPage(ActionEvent event) throws Exception{
         switchScene(event, "adminuser.fxml");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        HashMap<Integer, Double> yearlyStats;
+        XYChart.Series xyChart = new XYChart.Series<>();
+
+        AdminUser adminUser = (AdminUser) User.getUsers().get("adminuser@mail.com");
+        yearlyStats = adminUser.getYearlyStats().getYearlyStats();
+
+        for (int i = 1; i < 11; i++) {
+            xyChart.getData().add(new XYChart.Data<>(String.valueOf(i), yearlyStats.get(i)));
+        }
+        yearlyChart.getData().addAll(xyChart);
+
     }
 }
