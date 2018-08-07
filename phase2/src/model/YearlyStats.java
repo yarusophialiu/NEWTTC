7ec4a1c39dac3834cc8704516fe9e34c6cb4de9a
yaruliu <yarusophia@gmail.com>
2018-08-06 21:50:50 -0400

    create YearlyStats.java and add getter and setter of yearlystats

---
+++ phase2/src/model/YearlyStats.java
@@ -0,0 +1,22 @@
+package model;
+
+import java.util.HashMap;
+
+public class YearlyStats implements SystemStats{
+    private HashMap<Integer, Double> yearlyStats = new HashMap<Integer, Double>();
+
+    YearlyStats() {
+        for (int i = 1; i < 11; i++) {
+            yearlyStats.put(i, 0.0);
+
+        }
+    }
+
+    public HashMap<Integer, Double> getYearlyStats() {
+        return yearlyStats;
+    }
+
+    void updateYearlyStats(int year, double fare) {
+        yearlyStats.replace(year, yearlyStats.get(year) + fare);
+    }
+}
